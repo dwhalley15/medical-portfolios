7ff5ec2b60609e3e32ed18ee4dee4c69c58a6132
s5416741 <s5416741@bournemouth.ac.uk>
2025-02-16 10:21:12 +0000

    update/made a change to dynamic page so params are now treated as a promise

--- src/app/portfolios/[name]/page.tsx
+++ src/app/portfolios/[name]/page.tsx
@@ -1,21 +1,12 @@
-export default async function PortfolioPage({
-  params,
-}: {
-  params: { name: string };
-}) {
-  console.log("Params:", params);
+export default async function PortfolioPage({ params }: { params: Promise<{ name: string }> }) {
+  const resolvedParams = await params;
 
-  if (!params?.name) {
-    return <h1>Error: No name found</h1>;
-  }
   return (
-    <main className="page-container">
-      <section className="container">
-        <h1 className="blue">{`Welcome to ${params.name}'s Portfolio`}</h1>
-        <p className="blue">
-          {"This is a placeholder for the user's portfolio page."}
-        </p>
-      </section>
-    </main>
+      <main className="page-container">
+          <section className="container">
+              <h1 className="blue">{`Welcome to ${resolvedParams.name}'s Portfolio`}</h1>
+              <p className="blue">{"This is a placeholder for the user's portfolio page."}</p>
+          </section>
+      </main>
   );
 }
