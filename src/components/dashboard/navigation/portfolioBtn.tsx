9291b5996ff2a484a408573168e80a40bbd75c97
dwhalley15 <one_winged@hotmail.com>
2025-03-24 15:31:09 +0000

    feature/ added copy and share btns to dashboard.

--- src/components/dashboard/navigation/portfolioBtn.tsx
+++ src/components/dashboard/navigation/portfolioBtn.tsx
@@ -2,9 +2,22 @@
  * @file Portfolio Button
  * @description This file defines a button component for navigating to a user's portfolio.
  */
+"use client";
 
 import Link from "next/link";
-import Image from "next/image";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faXTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  LinkedinShareButton,
+} from "react-share";
 
 type PortfolioBtnProps = {
   portfolioUrl: string | null;
@@ -17,26 +30,81 @@ type PortfolioBtnProps = {
  * @returns {JSX.Element} The rendered component.
  */
 export default function PortfolioBtn({ portfolioUrl }: PortfolioBtnProps) {
+  const [copySuccess, setCopySuccess] = useState<string | null>(null);
 
-  if(!portfolioUrl) {
+  if (!portfolioUrl) {
     return null;
   }
 
+  const url = process.env.NEXT_PUBLIC_PUBLIC_URL;
+
+  /**
+   * @function handleCopy
+   * @description Copies the portfolio link to the user's clipboard.
+   * @returns {void}
+   */
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(`${url}/portfolios/${portfolioUrl}`)
+      .then(() => setCopySuccess("Link copied to clipboard!"))
+      .catch(() => setCopySuccess("Failed to copy link."));
+  };
+
   return (
-    <Link
-      href={`/portfolios/${portfolioUrl}`}
-      aria-label="Go to Portfolio page"
-      role="button"
-    >
-      <Image
-        src="https://frw6rziicw61rtm1.public.blob.vercel-storage.com/stethascope-JosqpnjSpMuNJlFlsJFAocilefG2PQ.jpg"
-        alt="An Image of a Stethascope"
-        className="shadow-border"
-        width={400}
-        height={400}
-        priority
-        quality={100}
-      />
-    </Link>
+    <div className="container">
+      <Link
+        href={`/portfolios/${portfolioUrl}`}
+        aria-label="Go to Portfolio page"
+        role="button"
+        className="link-text blue"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {`Click here to visit your portfolio page`}
+      </Link>
+      <div className="container btn-width">
+        <button
+          onClick={handleCopy}
+          aria-label="Copy portfolio link"
+          className="btn shadow-border blue-background btn-text white"
+        >
+          {"Copy Link"}
+          <FontAwesomeIcon icon={faCopy} aria-hidden="true" />
+        </button>
+
+        {copySuccess && <span className="blue normal-text">{copySuccess}</span>}
+
+        <div className="button-container">
+          <span className="blue normal-text">{"Share on:"}</span>
+
+          <FacebookShareButton
+            url={`${url}/portfolios/${portfolioUrl}`}
+            htmlTitle="Check out my portfolio!"
+            hashtag={`Check out my portfolio! #medicalportfolios ${url}/portfolios/${portfolioUrl}`}
+            className="share-btn facebook-icon-color"
+          >
+            <FontAwesomeIcon icon={faFacebook} aria-hidden="true" />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={`${url}/portfolios/${portfolioUrl}`}
+            htmlTitle="Check out my portfolio! #medicalportfolios"
+            className="share-btn"
+          >
+            <FontAwesomeIcon icon={faXTwitter} aria-hidden="true" />
+          </TwitterShareButton>
+
+          <LinkedinShareButton
+            url={`${url}/portfolios/${portfolioUrl}`}
+            htmlTitle="Check out my portfolio!"
+            summary="Check out my portfolio!"
+            source={`${url}/portfolios/${portfolioUrl}`}
+            className="share-btn facebook-icon-color"
+          >
+            <FontAwesomeIcon icon={faLinkedin} aria-hidden="true" />
+          </LinkedinShareButton>
+        </div>
+      </div>
+    </div>
   );
 }
